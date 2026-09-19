b22e9ac45192900e24a937d11daab4929ff66c6e
qasym13-debug <qasym13@icloud.com>
2026-09-19 18:13:26 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -167,11 +167,11 @@ async def my_id(update, context):
     await update.message.reply_text(f"🆔 Сіздің Telegram ID: <code>{update.effective_user.id}</code>", parse_mode="HTML")
 
 async def new(update, context):
-        if not await need_admin(update): return
-        db("UPDATE tournament SET status='finished' WHERE status='active'")
-        if not context.args or context.args[0] not in {"4","8","16","32"}:
-           await update.message.reply_text("Қолдану: /new 16")
-           return
+    if not await need_admin(update): return
+    db("UPDATE tournament SET status='finished' WHERE status='active'")
+    if not context.args or context.args[0] not in {"4","8","16","32"}:
+          await update.message.reply_text("Қолдану: /new 16")
+          return
     size = int(context.args[0])
     name = "NAIZA League"
     if len(context.args) > 1:
