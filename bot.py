fc2cd4836f1dd2169c51b015dfa9df2ec863ba70
qasym13-debug <qasym13@icloud.com>
2026-09-19 16:15:39 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -195,6 +195,9 @@ async def add(update, context):
     if not name:
         await update.message.reply_text("Қолдану: /add nz•Zevrix")
         return
+        if name in player_names(t["id"]):
+        await update.message.reply_text(f"⚠️ <b>{name}</b> есімді ойыншы турнирде бар!", parse_mode="HTML")
+        return
     count = len(player_names(t["id"]))
     if count >= t["size"]:
         await update.message.reply_text("❌ Ойыншы лимиті толды.")
