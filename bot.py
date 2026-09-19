6b095996fe706d8025bccf25f3d5ad219108b060
qasym13-debug <qasym13@icloud.com>
2026-09-19 18:14:48 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -170,8 +170,8 @@ async def new(update, context):
     if not await need_admin(update): return
     db("UPDATE tournament SET status='finished' WHERE status='active'")
     if not context.args or context.args[0] not in {"4","8","16","32"}:
-          await update.message.reply_text("Қолдану: /new 16")
-          return
+        await update.message.reply_text("Қолдану: /new 16")
+        return
     size = int(context.args[0])
     name = "NAIZA League"
     if len(context.args) > 1:
