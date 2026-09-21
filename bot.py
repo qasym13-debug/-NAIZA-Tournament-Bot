39d13d1bf75e85ccc9cc1dc1708d45f488ca3b95
qasym13-debug <qasym13@icloud.com>
2026-09-22 00:59:43 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -362,12 +362,13 @@ def main():
     app.run_webhook(
         listen="0.0.0.0",
         port=port,
-        url_path="webhook",
-        webhook_url=f"{external_url}/webhook",
-        drop_pending_updates=True,
+        url_path="",
+        webhook_url=f"{external_url}/",
+        drop_pending_updates=True
     )
 
 if __name__ == "__main__":
     main()
+
     
 
